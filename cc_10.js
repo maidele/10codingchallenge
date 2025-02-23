e3fdd4536bab076768389308aa244f58695aa469
Maidele Archelus <maidele@usf.edu>
2025-02-23 18:53:58 -0500

    Task 2 - Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -22,3 +22,27 @@ console.log(prod1.getDetails());
 prod1.updateStock(3);
 console.log(prod1.getDetails()); 
 // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 7"
+
+//Task 2-Creating an Order Class
+class Order {
+  constructor(orderID, product, quantity) {
+    this.orderID = orderID;
+    this.product = product;
+    this.quantity = quantity;
+    this.totalPriceIs = this.product.price*this.quantity
+    this.product.updateStock(this.quantity)
+  }// Creating a class for order and their properties
+   getOrderDetails() {
+    return(`OrderID:${this.orderID}, Product: ${this.product},  Quantity: ${this.quantity}, Total Price:${this.totalPriceIs}`);
+  
+  };// Including a "getOrderDetails()" method for stock's properties
+};
+
+
+const order1 = new Order(501, prod1, 2);
+console.log(order1.getOrderDetails());
+// Expected output: "Order ID: 501, Product: Laptop, Quantity: 2, Total Price: $2400"
+
+
+console.log(prod1.getDetails());
+// Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5" (Stock reduced)
