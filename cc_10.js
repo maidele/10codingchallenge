8823ec7917de71537cd5b350b2dfe6cecbfa3a8c
Maidele Archelus <maidele@usf.edu>
2025-02-23 17:18:44 -0500

    Task 1 - Created Employee Class

---
+++ cc_10.js
@@ -0,0 +1,24 @@
+//Task 1-Creating an Employee Class
+class Product {
+    constructor(name, id, price, stock) {
+      this.name = name;
+      this.id = id;
+      this.price=price;
+      this.stock=stock;
+    }// Creating a class for producy and their properties
+  
+    getDetails() {
+      return(`Product:${this.name}, ID: ${this.id}, Price: ${this.price}, Stock:${this.stock}`);
+    }// Including a "getDetails()" method for stock's properties
+
+    updateStock(quantity)  {
+    return this.stock -= quantity;
+  }// modifying the stock level
+};
+const prod1 = new Product("Laptop", 101, 1200, 10);
+console.log(prod1.getDetails()); 
+// Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 10"
+
+prod1.updateStock(3);
+console.log(prod1.getDetails()); 
+// Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 7"
